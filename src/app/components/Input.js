59a004c89f74a00defe6102a48c074ff02726f12
jmadrigal <wku.madrigal@gmail.com>
2024-07-10 00:43:14 -0500

    Created a Button Component to handle buttons on the site with "onClick" client side components. Allows for switching between button types.
    Wizard Form now has a base. Theme.js will hold our Themes selector component so we can pull it into admin panel, on wizard, and on Profile Page

--- src/app/components/Input.js
+++ src/app/components/Input.js
@@ -1,5 +1,6 @@
+import React from 'react'
 
-export const Input = ({ label, type, id, placeholder }) => {
+const Input = ({ label, type, id, placeholder }) => {
   return (
     <div className="flex flex-col w-full gap-2">
       <div className="flex justify-between">
@@ -16,3 +17,5 @@ export const Input = ({ label, type, id, placeholder }) => {
     </div>
   )
 }
+
+export default Input;
---
+++ src/app/components/Theme.js
@@ -0,0 +1,8 @@
+export default function Theme() {
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
