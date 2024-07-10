96e905dcef62e412508a346c7db547792f433a84
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-07-09 21:36:24 -0500

    home page updated/ responsive, new nav
    Co-authored-by: Dudeverse <Dudeverse@users.noreply.github.com>
    Co-authored-by: Paul Vickers <paul77uk@gmail.com>
    Co-authored-by: Ivan Rodriguez <IvanARodriguez@users.noreply.github.com>

--- src/app/components/product.js
+++ src/app/components/product.js
@@ -1,5 +1,6 @@
 "use client"
 import { useState, useEffect } from "react";
+import Image from 'next/image';
 // Fetch photos from JSONPlaceholder API or ANY API returning JSON
 const Product = () => {
     const [product, setProduct] = useState([]);
@@ -17,12 +18,17 @@ const Product = () => {
     }, []);
 
     return (
-        <div className="bg-gray-900 container mx-auto rounded-sm py-12 px-10 columns-3 sm:columns-4 md:columns-5 lg:columns-6 gap-5">
+        <div className="bg-gray-900 rounded-sm py-12 px-10 grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5">
             {product.map((prod) => {
                 return (
-                    <div className="basis-1/5 md:basis-1/6 m-5 lg:m-2 bg-gray-700 rounded-lg shadow-2xl overflow-hidden hover-lime" key={prod.id}>
-                        <a href="#" key={prod.id}>                        
-                            <img className="w-full h-full object-cover object-center" src={prod.thumbnailUrl} alt={prod.title} />
+                    <div className="basis-1/5 md:basis-1/6 m-5 lg:m-2 bg-gray-700 rounded-lg shadow-2xl hover-lime rounded overlow-hidden p-10" key={prod.id}>
+                        <a href="#" key={prod.id}> 
+                            <div className="flex justify-center items-center bg-green-600">
+                                <div className="w-full max-w-lg p-4">
+                                    <Image className="w-9/10 mx-auto rounded-full" layout="responsive" width={100} height={100} src={prod.thumbnailUrl} alt={prod.title} />
+                            </div>
+
+                            </div>
                             <div className="antialiased text-sm text-center text-gray-200 font-semibold tracking-wide p-6">
                                 <h3>{prod.title}</h3>
                                 <p className="text-gray-400">Price: $</p>
@@ -31,7 +37,7 @@ const Product = () => {
                                 <div className="antialiased text-sm text-center text-gray-200 font-semibold tracking-wide p-6 flex">
                                     <div className="">
                                         <h3>{prod.title}</h3>
-                                        <img className="" src={prod.url} />"
+                                        <Image className="w-full" width={100} height={100} alt={prod.title} src={prod.url} />
                                         <p className="text-gray-400">Price: $</p>
                                     </div>
                                     <div className="description"></div>
