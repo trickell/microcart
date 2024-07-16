74662eb8c9dc8f8843b725a11b8845085ef92f25
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-07-16 18:46:45 -0500

    -added some bits to the about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,8 +4,16 @@ export default function About() {
     return (
         <div className="about">
        <div>THE ONE AND ONLY MICROFIY</div>
+       <p>The cart that everyone <em>needed</em></p>
+       <br></br>
        <div>MISSION STATEMENT</div>
+       <p>To provide an easily integratable, affordable, and customizable cart design for online shops of all sizes and needs.</p>
+       <br></br>
        <div>HOW WE STARTED</div>
+       <p>The ambitious coders at 0 (1) Software network have banded together to create this magestic tool just for you.</p>
+       <br></br>
+       <code>2024 MICROFIY is born.</code>
        </div>
+       
     )
 }
